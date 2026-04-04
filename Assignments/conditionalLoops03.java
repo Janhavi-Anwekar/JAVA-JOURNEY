ff3bd96e5676f92570652bbe92d5e0e287f46f1a
Janhavi-Anwekar <anwekar.janhavi2019@gmail.com>
2026-04-04 21:51:33 +0530

    Upadated, made functions for each question instead writing each solution in main function

--- Assignments/conditionalLoops03.java
+++ Assignments/conditionalLoops03.java
@@ -1,19 +1,19 @@
 import java.util.Scanner;
 
 public class conditionalLoops03 {
-    
-    public static void main(String[] args) {
-        
-        // 23. Input a number and print all the factors of that number (use loops)
-        int a = 64;
+
+    // 23. Input a number and print all the factors of that number (use loops)
+    public static void printFactors(int a) {
         for(int i=a; i>0; i--) {
             if(a % i == 0) {
                 System.out.print(i+" ");
             }
         }
         System.out.println();
+    }
 
-        //24. Take integer inputs till the user enters 0 and print the sum of all numbers (HINT: while loop)
+    //24. Take integer inputs till the user enters 0 and print the sum of all numbers (HINT: while loop)
+    public static void printSum() {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter n : ");
         int n = sc.nextInt();
@@ -25,34 +25,35 @@ public class conditionalLoops03 {
             sum += n;
         }
         System.out.println("SUM : "+ sum);
+    }
 
-        // Intermediate Java Program
 
-        //7. Power In Java
-        int x= 5;  //base
-        int p = 3;   // power
+    // Intermediate Java Program
 
+    //7. Power In Java
+    public static void xPown(int x, int p) {
         int xRp = (int)Math.pow(x, p);
         System.out.println(xRp);
+    }
 
-        //8. Calculate Depreciation of Value
-        // Depreciation per year= (Cost − Salvage Value​) / Useful Life
+    //8. Calculate Depreciation of Value
+    // Depreciation per year= (Cost − Salvage Value​) / Useful Life
 
-        // 9. Calculate Batting Average
-        // Batting Average = Total Runs Scored / Number of Times Out
+    // 9. Calculate Batting Average
+    // Batting Average = Total Runs Scored / Number of Times Out
         
-        // 10. Calculate CGPA Java Program
-        // CGPA = ∑(Grade Points×Credits)​​ / ∑Credits
+    // 10. Calculate CGPA Java Program
+    // CGPA = ∑(Grade Points×Credits)​​ / ∑Credits
 
-        //11. Compound Interest Java Program
-        // get formula and calculate
+    //11. Compound Interest Java Program
+    // get formula and calculate
 
-        //15. Find Ncr & Npr
-        // NCR = (n−r)!n!​ / r!
-        // NPR = n! / (n−r)!
-        
-        //16. Reverse A String In Java
-        String s = "video";
+    //15. Find Ncr & Npr
+    // NCR = (n−r)!n!​ / r!
+    // NPR = n! / (n−r)!
+
+    //16. Reverse A String In Java
+    public static void revStr(String s) {
         int len = s.length();
         String newS = "";
         while(len > 0) {
@@ -61,7 +62,39 @@ public class conditionalLoops03 {
         }
 
         System.out.println(newS);
+    }
 
+    //17. Find if a number is palindrome or not
+    public static boolean isPalindrome(int num) {
+
+        
+        if(num < 0 || num % 10 == 0 && num != 0) {
+            return false;
+        }
+        int revN = 0;
+        while(num > revN){
+            revN = revN*10 + (num%10);
+            num = num/10;
+        }
+
+        if(revN == num || revN/10 == num){
+            return true;
+        }
+
+        return false;
+    } 
+    
+    public static void main(String[] args) {
+        
+        
+        System.out.println(isPalindrome(121));
+        
+        
+        
+       
+        
+
+        
 
     }
 }
